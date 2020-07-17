7d77ff365d3310f657cc05ec197620a5c8e2495a
shubhamdwivedii <shubham00d@gmail.com>
2020-07-17 18:43:51 +0530

    added build also

---
+++ build/precache-manifest.a7201d0abbf11bf8ec46dc613b62dced.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6fc7beb10da108a1b407619bd2b549",
+    "url": "/square-pattern-memorization-game/index.html"
+  },
+  {
+    "revision": "90b3e524df3560b7cb73",
+    "url": "/square-pattern-memorization-game/static/css/main.27f207e5.chunk.css"
+  },
+  {
+    "revision": "5c2eb0cfe9649e5443f4",
+    "url": "/square-pattern-memorization-game/static/js/2.04f7041c.chunk.js"
+  },
+  {
+    "revision": "b4fd4f100124e08e569897970dcb07ed",
+    "url": "/square-pattern-memorization-game/static/js/2.04f7041c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b3e524df3560b7cb73",
+    "url": "/square-pattern-memorization-game/static/js/main.0c6bda31.chunk.js"
+  },
+  {
+    "revision": "60624bad60c573ab2e5f",
+    "url": "/square-pattern-memorization-game/static/js/runtime-main.a4dc38d7.js"
+  },
+  {
+    "revision": "f4832018244d103054b7a8b44d1a9112",
+    "url": "/square-pattern-memorization-game/static/media/check.f4832018.mp3"
+  },
+  {
+    "revision": "2f3596787b6ee9cde7bcc7c471bf72d8",
+    "url": "/square-pattern-memorization-game/static/media/clear.2f359678.mp3"
+  },
+  {
+    "revision": "31a04400b3776752f60937b07a807a91",
+    "url": "/square-pattern-memorization-game/static/media/star.31a04400.png"
+  },
+  {
+    "revision": "d667237d079a127dfcc8a33d8a4c07de",
+    "url": "/square-pattern-memorization-game/static/media/strike.d667237d.mp3"
+  }
+]);
